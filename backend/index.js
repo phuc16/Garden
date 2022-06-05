b6018c13161dffe897ed4daaa2f12837fe0866cd
TienMinh <tienminh0801@gmail.com>
2022-06-05 22:40:02 +0700

    deploy frontend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 
 var allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');

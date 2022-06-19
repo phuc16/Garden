6a6c1f20b5aadb42fefa24a8ca662a6323813891
TienMinh <tienminh0801@gmail.com>
2022-06-19 13:46:44 +0700

    fix data last

--- backend/app/controllers/dataController.js
+++ backend/app/controllers/dataController.js
@@ -31,19 +31,42 @@ exports.getDatabyGarden = (req, res) => {
 
 
 exports.getAllLastData = (req, res) => {
-    db.query(`SELECT * FROM datas WHERE time IN (SELECT MAX(time) FROM datas GROUP BY category)` , (err,result) => {
+    db.query(`SELECT category FROM datas GROUP BY category` , (err,result) => {
         if (err) res.send(err)
         else {
-            if (result.rows.length == 0 ) res.send({"message" : "Data not found"})
-            else {
-                for (let data of result.rows) {
-                    data.time = data.time.toLocaleString('en-GB')
-                }
-                res.send(result.rows)
+            const response = []
+            let categories = result.rows
+            let len = categories.length
+            for (let i = 0; i< len; i++){
+                db.query(`SELECT * FROM datas WHERE category = '${categories[i].category}' ORDER BY time DESC LIMIT 1 `, (error, data) => {
+                    if (error) console.log(error)
+                    else {
+                        if (result.rows.length == 0 ) {
+                            res.send({"message" : "Data not found"})
+                            return
+                        }
+                        (data.rows)[0].time = (data.rows)[0].time.toLocaleString('en-GB')
+                        response.push((data.rows)[0])
+
+                        if ( i == len - 1) res.send(response)
+                    }
+                })
             }
         }
-        
     })
+    // db.query(`SELECT * FROM datas WHERE time IN (SELECT MAX(time) FROM datas GROUP BY category)` , (err,result) => {
+    //     if (err) res.send(err)
+    //     else {
+    //         if (result.rows.length == 0 ) res.send({"message" : "Data not found"})
+    //         else {
+    //             for (let data of result.rows) {
+    //                 data.time = data.time.toLocaleString('en-GB')
+    //             }
+    //             res.send(result.rows)
+    //         }
+    //     }
+        
+    // })
 }
 
 
